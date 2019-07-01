f36005836b8c517d8980c708195c441d04a577ec
Roy Weller <mail@royweller.co.uk>
2019-07-01 23:28:56 +0000

    Merged PR 9: Build and release pipeline

--- karma.conf.js
+++ karma.conf.js
@@ -10,22 +10,39 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/raw-portfolio'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    reporters: ['progress', 'junit', 'kjhtml'],
+    junitReporter: {
+      outputDir: 'test', // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'results.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      useBrowserName: false, // add browser name to report and classes names
     },
-    reporters: ['progress', 'kjhtml'],
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './test'),
+      reports: ['cobertura'],
+      fixWebpackSourcePaths: true,
+      'report-config': {
+          cobertura: {
+            file: 'coverage.xml'
+          }
+      }
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
